a91620a0ccaa09b0e2886db04fa3db658b3f74ec
unknown <calvinp003@gmail.com>
2020-06-05 16:31:02 -0700

    add basic animation Co-authored-by: Ali Amirhamzeh <53623986+aliamirh@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import kickWindup2 from './goku sprites/kick windup2.png'
 import idle from './goku sprites/idle 1.png'
 
 function App() {
-  const [currentStatus, setCurrentStatus] = useState('idle')
-
-  let goku;
+  const [currentStatus, setCurrentStatus] = useState(idle)
+ 
+  let goku = currentStatus;
   const kickWindup = [kickWindup1, kickWindup2];
-
-  if (currentStatus === 'idle'){
-      goku = idle
+  const kickAnimation = () => {
+    for (let i=0; i < kickWindup.length; i ++){
+      setTimeout(function() {
+        setCurrentStatus(kickWindup[i])
+      }, 400 * i)
+    }
   }
+
+  if (currentStatus === idle){
+    kickAnimation();
+  }
+
+
   return (
     <div className="App">
       <img src={goku}/>
